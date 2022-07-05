85f486beaa4dca14c5982ef361e53ccf4f772216
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-07-05 10:32:45 +0200

    Could this be it

--- config/provider.go
+++ config/provider.go
@@ -20,10 +20,9 @@ import (
 	// Note(turkenh): we are importing this to embed provider schema document
 	_ "embed"
 
+	iamgroup "github.com/crossplane-contrib/provider-jet-hsdp/config/iamgroup"
 	tjconfig "github.com/crossplane/terrajet/pkg/config"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-
-	iamgroup "github.com/crossplane-contrib/provider-jet-hsdp/config/iam_group"
 )
 
 const (
