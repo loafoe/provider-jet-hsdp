ef7ab0df36099a40245fe876bf17b46f51ffdb37
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-07-05 10:19:24 +0200

    Fix CRD name / DNS issue

--- config/iam_group/config.go
+++ config/iam_group/config.go
@@ -5,6 +5,6 @@ import "github.com/crossplane/terrajet/pkg/config"
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("hsdp_iam_group", func(r *config.Resource) {
-		r.ExternalName = config.NameAsIdentifier
+		r.ShortGroup = "iamGroup"
 	})
 }
